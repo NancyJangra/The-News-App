c877871aa2ddf04f385cac1a64d45abb3251f499
pranaveechandra <chandrapranavee@gmail.com>
2025-04-19 22:28:03 +0530

    Pranavee 3

--- app/src/main/java/com/example/thenewsapplication/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/thenewsapplication/adapters/NewsAdapter.kt
@@ -3,7 +3,10 @@ package com.example.thenewsapp.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.newsprojectpractice.R
 
 class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewModel>() {
 
@@ -13,7 +16,10 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewModel>() {
     lateinit var articleTitle: TextView
     lateinit var articleDescription: TextView
     lateinit var articleDateTime: TextView
-    private val differCallback = object : DiffUtil.ItemCallback<Article>()
+    private val differCallback = object : DiffUtil.ItemCallback<Article>(){
+
+    }
+    val differ = AsyncListDiffer(adapter: this, differCallback)
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewModel {
         return ArticleViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent, attachToRoot: false)
@@ -21,10 +27,24 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewModel>() {
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return differ.currentList.size
     }
-
+    private var onItemClickListener: ((Article) -> Unit)? = null
     override fun onBindViewHolder(holder: ArticleViewModel, position: Int) {
-        TODO("Not yet implemented")
+        val article =  differ.currentList[position]
+        articleImage = holder.itemView.findViewById<>(R.id.articleImage)
+
+        articleSource.text = article.source?.name
+        articleTitle.text = article.title
+        articleDescription.text = article.description
+        articleDateTime.text = article.publishedAt
+
+        onItemClickListener?.let {
+            it(article)
+        }
+
+    }
+    fun setOnItemClickListener(listener: (Article) -> Unit){
+        onItemClickListener = listener
     }
 }
