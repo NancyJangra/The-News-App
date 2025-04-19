a6f383802b7e49664d7c735c729b02484caece3a
pranaveechandra <chandrapranavee@gmail.com>
2025-04-19 18:56:05 +0530

    Pranavee 2

--- app/src/main/java/com/example/thenewsapplication/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/thenewsapplication/adapters/NewsAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.thenewsapp.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewModel>() {
@@ -11,4 +13,18 @@ class NewsAdapter: RecyclerView.Adapter<NewsAdapter.ArticleViewModel>() {
     lateinit var articleTitle: TextView
     lateinit var articleDescription: TextView
     lateinit var articleDateTime: TextView
+    private val differCallback = object : DiffUtil.ItemCallback<Article>()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewModel {
+        return ArticleViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_news,parent, attachToRoot: false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewModel, position: Int) {
+        TODO("Not yet implemented")
+    }
 }
