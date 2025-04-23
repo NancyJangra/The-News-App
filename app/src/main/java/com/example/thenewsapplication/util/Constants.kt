52589877a36dadbf5fb2c595831f112e870395d7
NaliniBhardwaj <nalinibhar325111@gmail.com>
2025-04-24 01:22:45 +0530

    evdv

--- app/src/main/java/com/example/thenewsapplication/util/Constants.kt
+++ app/src/main/java/com/example/thenewsapplication/util/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object{
         const val API_KEY = "0fb7ed0db54e4824af713be46990fc73"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_TIME_NEWS_DELAY = 500L
+        const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
 
     }
